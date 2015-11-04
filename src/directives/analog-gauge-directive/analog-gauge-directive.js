b00c95c57d35e874330bf662ce1e4dcd69a61238
Robert Wesołowski <kauczuk79@gmail.com>
2015-11-04 17:32:00 +0100

    Analog gauge directive

--- src/directives/analog-gauge-directive/analog-gauge-directive.js
+++ src/directives/analog-gauge-directive/analog-gauge-directive.js
@@ -1,17 +1,5 @@
 (function () {
     'use strict';
-    /*global angular*/
-
-    angular.module('dashboard-ui', ['ngRoute', 'dashboard-ui.directives']);
-}());
-(function () {
-    'use strict';
-    /*global angular*/
-
-    angular.module('dashboard-ui.directives', []);
-}());
-(function () {
-    'use strict';
     /*global angular, console*/
 
     function AnalogGaugeDirective() {
@@ -70,72 +58,4 @@
     angular
         .module('dashboard-ui.directives')
         .directive('analogGauge', AnalogGaugeDirective);
-}());
-(function () {
-    'use strict';
-    /*global angular, console*/
-
-    function FourteenSegmentDisplayDirective() {
-        function link(scope, element, attrs) {
-            var digits = scope.digits,
-                value = scope.value,
-                background = (attrs.showBackground === "true"),
-                iterator;
-            scope.background = '';
-            if (background) {
-                scope.background = '~';
-                for (iterator = 0; iterator < digits - 1; iterator += 1) {
-                    scope.background += '.~';
-                }
-            }
-        }
-
-        return {
-            link: link,
-            restrict: 'E',
-            template: '<span class="background">{{background}}</span><span class="foreground">{{value}}</span>',
-            scope: {
-                digits: '@',
-                value: '@'
-            }
-        };
-    }
-
-    angular
-        .module('dashboard-ui.directives')
-        .directive('fourteenSegmentDisplay', FourteenSegmentDisplayDirective);
-}());
-(function () {
-    'use strict';
-    /*global angular, console*/
-
-    function SevenSegmentDisplayDirective() {
-        function link(scope, element, attrs) {
-            var digits = scope.digits,
-                value = scope.value,
-                background = (attrs.showBackground === "true"),
-                iterator;
-            scope.background = '';
-            if (background) {
-                scope.background = '8';
-                for (iterator = 0; iterator < digits - 1; iterator += 1) {
-                    scope.background += '.8';
-                }
-            }
-        }
-
-        return {
-            link: link,
-            restrict: 'E',
-            template: '<span class="background">{{background}}</span><span class="foreground">{{value}}</span>',
-            scope: {
-                digits: '@',
-                value: '@'
-            }
-        };
-    }
-
-    angular
-        .module('dashboard-ui.directives')
-        .directive('sevenSegmentDisplay', SevenSegmentDisplayDirective);
 }());
