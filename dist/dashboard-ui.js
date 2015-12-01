37d08de9d4cd33d56a6ee41d93750b4a35128dd2
Robert Wesołowski <kauczuk79@gmail.com>
2015-12-01 17:32:17 +0100

    Unit tests fixed

--- dist/dashboard-ui.js
+++ dist/dashboard-ui.js
@@ -235,69 +235,6 @@
         .directive('alphanumericLcd', AlphanumericLcdDirective);
 } (window.d3));
 (function (d3) {
-    'use strict';
-    /*global angular, console*/
-
-    function AnalogGaugeDirective() {
-        function link(scope, element, attrs) {
-            var x = scope.x || 0.0,
-                y = scope.y || 0.0,
-                gaugeGroup = d3.select(element[0]),
-                indicator = gaugeGroup.select('#indicator'),
-                indicatorBoundingBox = indicator.node().getBoundingClientRect(),
-                svgBBox = d3.select('svg').node().getBoundingClientRect(),
-                angle,
-                deltaAngle,
-                deltaValue;
-            function updateGaugeAngle() {
-                var value = scope.value;
-                if (value < scope.minValue) {
-                    angle = scope.startAngle;
-                } else if (value > scope.maxValue) {
-                    angle = scope.endAngle;
-                } else {
-                    var angleDifference = Math.abs((deltaAngle / deltaValue) * (scope.minValue - value));
-                    if (scope.startAngle < scope.endAngle) {
-                        angle = scope.startAngle + angleDifference;
-                    } else {
-                        angle = scope.startAngle - angleDifference;
-                    }
-                }
-                indicator.rotate(angle);
-            }
-            scope.indicatorOriginX = scope.indicatorOriginX || ((indicatorBoundingBox.left + indicatorBoundingBox.right) / 2) - svgBBox.left;
-            scope.indicatorOriginY = scope.indicatorOriginY || (indicatorBoundingBox.bottom - svgBBox.top);
-            gaugeGroup.prependTranslate(x, y);
-            scope.endAngle = scope.endAngle || (scope.startAngle * -1);
-            scope.minValue = scope.minValue || 0;
-            deltaAngle = scope.endAngle - scope.startAngle;
-            deltaValue = scope.maxValue - scope.minValue;
-            indicator.transformOrigin(scope.indicatorOriginX, scope.indicatorOriginY).style('transition', 'all 0.25s linear');
-            scope.$watch('value', updateGaugeAngle);
-        }
-
-        return {
-            link: link,
-            restrict: 'C',
-            scope: {
-                value: '=',
-                startAngle: '=',
-                endAngle: '=',
-                maxValue: '=',
-                minValue: '=',
-                indicatorOriginX: '=',
-                indicatorOriginY: '=',
-                x: '=',
-                y: '='
-            }
-        };
-    }
-
-    angular
-        .module('dashboard-ui.directives')
-        .directive('analogGauge', AnalogGaugeDirective);
-} (window.d3));
-(function (d3) {
 	'use strict';
 	/*global angular, console*/
 
@@ -375,6 +312,69 @@
 	angular
 		.module('dashboard-ui.directives')
 		.directive('barMeter', BarMeterDirective);
+} (window.d3));
+(function (d3) {
+    'use strict';
+    /*global angular, console*/
+
+    function AnalogGaugeDirective() {
+        function link(scope, element, attrs) {
+            var x = scope.x || 0.0,
+                y = scope.y || 0.0,
+                gaugeGroup = d3.select(element[0]),
+                indicator = gaugeGroup.select('#indicator'),
+                indicatorBoundingBox = indicator.node().getBoundingClientRect(),
+                svgBBox = d3.select('svg').node().getBoundingClientRect(),
+                angle,
+                deltaAngle,
+                deltaValue;
+            function updateGaugeAngle() {
+                var value = scope.value;
+                if (value < scope.minValue) {
+                    angle = scope.startAngle;
+                } else if (value > scope.maxValue) {
+                    angle = scope.endAngle;
+                } else {
+                    var angleDifference = Math.abs((deltaAngle / deltaValue) * (scope.minValue - value));
+                    if (scope.startAngle < scope.endAngle) {
+                        angle = scope.startAngle + angleDifference;
+                    } else {
+                        angle = scope.startAngle - angleDifference;
+                    }
+                }
+                indicator.rotate(angle);
+            }
+            scope.indicatorOriginX = scope.indicatorOriginX || ((indicatorBoundingBox.left + indicatorBoundingBox.right) / 2) - svgBBox.left;
+            scope.indicatorOriginY = scope.indicatorOriginY || (indicatorBoundingBox.bottom - svgBBox.top);
+            gaugeGroup.prependTranslate(x, y);
+            scope.endAngle = scope.endAngle || (scope.startAngle * -1);
+            scope.minValue = scope.minValue || 0;
+            deltaAngle = scope.endAngle - scope.startAngle;
+            deltaValue = scope.maxValue - scope.minValue;
+            indicator.transformOrigin(scope.indicatorOriginX, scope.indicatorOriginY).style('transition', 'all 0.25s linear');
+            scope.$watch('value', updateGaugeAngle);
+        }
+
+        return {
+            link: link,
+            restrict: 'C',
+            scope: {
+                value: '=',
+                startAngle: '=',
+                endAngle: '=',
+                maxValue: '=',
+                minValue: '=',
+                indicatorOriginX: '=',
+                indicatorOriginY: '=',
+                x: '=',
+                y: '='
+            }
+        };
+    }
+
+    angular
+        .module('dashboard-ui.directives')
+        .directive('analogGauge', AnalogGaugeDirective);
 } (window.d3));
 (function (d3) {
 	'use strict';
--- tests/karma/analog-gauge-directive.js
+++ tests/karma/analog-gauge-directive.js
@@ -1,4 +1,4 @@
-/*global describe, it, beforeEach, inject, expect, d3*/
+/*global describe, it, beforeEach, inject, expect, d3, spyOn*/
 describe('Analog gauge directive', function () {
 	var element,
 		scope;
@@ -18,20 +18,29 @@ describe('Analog gauge directive', function () {
 		$compile = _$compile_;
 		$scope = _$rootScope_.$new();
 		d3.selection.prototype.node = d3_selection_prototype_node;
-		element = angular.element('<g class="analog-gauge" data-max-value="100" data-start-angle="-100" data-value="50"><g id="indicator"><rect y="15" x="97.5" width="5" height="85" fill="black"></rect></g></g>');
+		element = angular.element('<g class="analog-gauge" data-max-value="100" data-start-angle="-100" data-value="50" data-indicator-origin-x="100" data-indicator-origin-y="100"><g id="indicator"><rect y="15" x="97.5" width="5" height="85" fill="black"></rect></g></g>');
 		$compile(element)($scope);
 		$scope.$digest();
 		scope = element.isolateScope();
 	}));
 
-	it('should has got proper values of not requred parameters', function () {
+	it('should have proper values of not requred (calculated) parameters', function () {
 		expect(scope.endAngle).toEqual(100);
 		expect(scope.minValue).toEqual(0);
 	});
 
-	it('should has got proper values of given parameters', function () {
+	it('should have proper values of given parameters', function () {
 		expect(scope.value).toEqual(50);
 		expect(scope.startAngle).toEqual(-100);
 		expect(scope.maxValue).toEqual(100);
+		expect(scope.indicatorOriginX).toEqual(100);
+		expect(scope.indicatorOriginY).toEqual(100);
+	});
+	
+	it('should call updateGaugeAngle when data-value was changed', function() {
+		spyOn(scope.$$watchers[0], 'fn').and.callThrough();
+		scope.value = 10;
+		scope.$digest();
+		expect(scope.$$watchers[0].fn).toHaveBeenCalled();
 	});
 });
