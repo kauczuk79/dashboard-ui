1fc1ce9fa03ec40225a888831df0dc42c6d035be
Robert Wesołowski <kauczuk79@gmail.com>
2015-12-03 12:46:01 +0100

    Fix all attributes test in alphanumeric lcd unit test

--- tests/unit/alphanumeric-lcd-directive.spec.js
+++ tests/unit/alphanumeric-lcd-directive.spec.js
@@ -1,6 +1,6 @@
 describe('Alphanumeric LCD directive', function () {
 	var HTML_WITH_REQUIRED_ATTRIBUTES = '<g class="alphanumeric-lcd" data-lines="[\'test\']"></g>',
-		HTML_WITH_ALL_ATTRIBUTES = '<g class="alphanumeric-lcd" data-lines="[\'test\']"></g>',
+		HTML_WITH_ALL_ATTRIBUTES = '<g class="alphanumeric-lcd" data-lines="[\'test\']" data-x="10" data-y="10" data-columns="24" data-rows="3" data-show-background="true" data-scale="1.5"></g>',
 		$compile,
 		$scope;
 	beforeEach(module('dashboard-ui'));
@@ -23,12 +23,12 @@ describe('Alphanumeric LCD directive', function () {
 	it('should have proper values of given parameters', function () {
 		var scope = getElementScope(HTML_WITH_ALL_ATTRIBUTES, $compile, $scope);
 		expect(scope.lines).toEqual(['test']);
-		expect(scope.x).toEqual(0);
-		expect(scope.y).toEqual(0);
-		expect(scope.columns).toEqual(16);
-		expect(scope.rows).toEqual(2);
-		expect(scope.showBackground).toEqual(false);
-		expect(scope.scale).toEqual(1.0);
+		expect(scope.x).toEqual(10);
+		expect(scope.y).toEqual(10);
+		expect(scope.columns).toEqual(24);
+		expect(scope.rows).toEqual(3);
+		expect(scope.showBackground).toEqual(true);
+		expect(scope.scale).toEqual(1.5);
 	});
 
 	it('should call updateGaugeAngle when data-value was changed', function () {
