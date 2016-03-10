69a4723e58b59c3b31e94d95831d2fa3b2862e1a
Robert Wesołowski <kauczuk79@gmail.com>
2016-03-10 22:26:46 +0100

    Alphanumeric lcd and analog gauge directives was refactored, parameter's parsing and assigning to variables

--- src/directives/alphanumeric-lcd-directive/alphanumeric-lcd-directive.js
+++ src/directives/alphanumeric-lcd-directive/alphanumeric-lcd-directive.js
@@ -10,33 +10,33 @@
                 lcdGroup = d3.select(element[0]),
                 lineIterator,
                 fontHeight = 18,
-                yPosition;
+                yPosition,
+                rows = parseInt(scope.rows, 10) || 2,
+                columns = parseInt(scope.columns, 10) || 16,
+                scale = parseFloat(scope.scale) || 1.0,
+                x = parseFloat(scope.x) || 0,
+                y = parseFloat(scope.y) || 0,
+            showBackground = scope.showBackground || false;
             function updateLines() {
                 var lineNumber,
                     lines = scope.lines;
-                for (lineNumber = 0; lineNumber < scope.rows; lineNumber += 1) {
+                for (lineNumber = 0; lineNumber < rows; lineNumber += 1) {
                     if (lines[lineNumber] !== undefined) {
                         d3.select(element[0]).selectAll('.' + FOREGROUND_CLASS).data(lines).text(function (data) {
-                            return data.substring(0, scope.columns);
+                            return data.substring(0, columns);
                         });
                     }
                 }
             }
-            scope.rows = scope.rows || 2;
-            scope.columns = scope.columns || 16;
-            scope.scale = scope.scale || 1.0;
-            scope.x = scope.x || 0;
-            scope.y = scope.y || 0;
-            scope.showBackground = scope.showBackground || false;
             scope.$watch('lines', updateLines);
-            lcdGroup.prependTranslate(scope.x, scope.y);
-            for (lineIterator = 0; lineIterator < scope.rows; lineIterator += 1) {
+            lcdGroup.prependTranslate(x, y);
+            for (lineIterator = 0; lineIterator < rows; lineIterator += 1) {
                 yPosition = fontHeight * (lineIterator + 1);
-                lcdGroup.append('text').classed(FOREGROUND_CLASS, true).attr('y', yPosition).prependScale(scope.scale);
-                if (scope.showBackground) {
-                    lcdGroup.append('text').classed(BACKGROUND_CLASS, true).attr('y', yPosition).prependScale(scope.scale).data(RECTANGLE_CHAR).text(function (data) {
+                lcdGroup.append('text').classed(FOREGROUND_CLASS, true).attr('y', yPosition).prependScale(scale);
+                if (showBackground) {
+                    lcdGroup.append('text').classed(BACKGROUND_CLASS, true).attr('y', yPosition).prependScale(scale).data(RECTANGLE_CHAR).text(function (data) {
                         var arr = [];
-                        arr.length = scope.columns + 1;
+                        arr.length = columns + 1;
                         return arr.join(data);
                     });
                 }
@@ -47,13 +47,13 @@
             link: link,
             restrict: 'C',
             scope: {
-                rows: '=',
-                columns: '=',
-                scale: '=',
-                showBackground: '=',
+                rows: '@',
+                columns: '@',
+                scale: '@',
+                showBackground: '@',
                 lines: '=',
-                x: '=',
-                y: '='
+                x: '@',
+                y: '@'
             }
         };
     }
--- src/directives/analog-gauge-directive/analog-gauge-directive.js
+++ src/directives/analog-gauge-directive/analog-gauge-directive.js
@@ -8,35 +8,33 @@
                 indicator = gaugeGroup.select('#indicator'),
                 indicatorBoundingBox = indicator.node().getBoundingClientRect(),
                 svgBBox = d3.select('svg').node().getBoundingClientRect(),
-                angle,
-                deltaAngle,
-                deltaValue;
+                x = parseFloat(scope.x) || 0.0,
+                y = parseFloat(scope.y) || 0.0,
+                indicatorOriginX = parseFloat(scope.indicatorOriginX) || ((indicatorBoundingBox.left + indicatorBoundingBox.right) / 2) - svgBBox.left,
+                indicatorOriginY = parseFloat(scope.indicatorOriginY) || (indicatorBoundingBox.bottom - svgBBox.top),
+                startAngle = parseFloat(scope.startAngle) || 0.0,
+                endAngle = parseFloat(scope.endAngle) || (startAngle * -1),
+                minValue = parseFloat(scope.minValue) || 0,
+                maxValue = parseFloat(scope.maxValue) || 100.0,
+                deltaAngle = endAngle - startAngle,
+                deltaValue = maxValue - minValue;
             function updateGaugeAngle() {
-                var value = scope.value;
-                if (value < scope.minValue) {
-                    angle = scope.startAngle;
-                } else if (value > scope.maxValue) {
-                    angle = scope.endAngle;
+                var value = parseFloat(scope.value);
+                if (value < minValue) {
+                    indicator.rotate(startAngle);
+                } else if (value > maxValue) {
+                    indicator.rotate(endAngle);
                 } else {
-                    var angleDifference = Math.abs((deltaAngle / deltaValue) * (scope.minValue - value));
-                    if (scope.startAngle < scope.endAngle) {
-                        angle = scope.startAngle + angleDifference;
+                    var angleDifference = Math.abs((deltaAngle / deltaValue) * (minValue - value));
+                    if (startAngle < endAngle) {
+                        indicator.rotate(startAngle + angleDifference);
                     } else {
-                        angle = scope.startAngle - angleDifference;
+                        indicator.rotate(startAngle - angleDifference);
                     }
                 }
-                indicator.rotate(angle);
             }
-            scope.x = scope.x || 0.0;
-            scope.y = scope.y || 0.0;
-            scope.indicatorOriginX = scope.indicatorOriginX || ((indicatorBoundingBox.left + indicatorBoundingBox.right) / 2) - svgBBox.left;
-            scope.indicatorOriginY = scope.indicatorOriginY || (indicatorBoundingBox.bottom - svgBBox.top);
-            gaugeGroup.prependTranslate(scope.x,scope.y);
-            scope.endAngle = scope.endAngle || (scope.startAngle * -1);
-            scope.minValue = scope.minValue || 0;
-            deltaAngle = scope.endAngle - scope.startAngle;
-            deltaValue = scope.maxValue - scope.minValue;
-            indicator.transformOrigin(scope.indicatorOriginX, scope.indicatorOriginY).style('transition', 'all 0.25s linear');
+            gaugeGroup.prependTranslate(x,y);
+            indicator.transformOrigin(indicatorOriginX, indicatorOriginY).style('transition', 'all 0.25s linear');
             scope.$watch('value', updateGaugeAngle);
         }
 
@@ -45,14 +43,14 @@
             restrict: 'C',
             scope: {
                 value: '=',
-                startAngle: '=',
-                endAngle: '=',
-                maxValue: '=',
-                minValue: '=',
-                indicatorOriginX: '=',
-                indicatorOriginY: '=',
-                x: '=',
-                y: '='
+                startAngle: '@',
+                endAngle: '@',
+                maxValue: '@',
+                minValue: '@',
+                indicatorOriginX: '@',
+                indicatorOriginY: '@',
+                x: '@',
+                y: '@'
             }
         };
     }
